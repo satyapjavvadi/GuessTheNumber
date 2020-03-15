d41230ab4356aa00d75aac13330f46e0c95895c5
Satya P Javvadi <saphin.rao@gmail.com>
2020-03-14 20:50:11 -0700

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        int theNumber;
+        int max;     
+
+        public Program()
+        {
+            Random rand = new Random();
+             max = 100;
+             theNumber = Math.Abs(rand.Next() % (max + 1));
+        }
+
+        public void play()
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                int move = Convert.ToInt32(input);
+
+                if (move > theNumber)
+                {
+                    Console.WriteLine("Your number is pretty big");
+                }
+                else if (move < theNumber)
+                {
+                    Console.WriteLine("Your number is pretty small");
+                }
+                else
+                {
+                    Console.WriteLine("You got it");
+                    break;
+                }
+            }
+
+        }       
+        public static void Main(string[] args)
+        {
+            Program guessGame = new Program();
+            Console.WriteLine($"Welcome to my game. Try and guess in between 0 and 100. Just type a number to get started");
+            guessGame.play();
+        }
+    }
+}
